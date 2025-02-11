fc8d8221e5c5b7df5d553b22d726b2646b2ed713
Dylan <dylanebp@hotmail.com>
2025-02-11 23:18:58 +0000

    Added dependencies to requirements.txt, fixed bug in install script

--- install-script.sh
+++ install-script.sh
@@ -1,11 +1,18 @@
+# Install Syft and Grype
 curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sudo sh -s -- -b /usr/local/bin
 curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sudo sh -s -- -b /usr/local/bin
+
+# Download and process wad2-assign2 repo
 curl -L -o main.zip https://github.com/DylBP/wad2-assign2/archive/refs/heads/main.zip
 unzip main.zip
-syft wad2-assign2-main/ -o cyclonedx-json > sbom.json
-cat sbom.json | grype > output.grype
+cd wad2-assign2-main
+syft . -o cyclonedx-json > sbom.json
+grype sbom.json -o table > output.grype
+cd ..
 
+# Download and run SBOM-FYP Flask API
 curl -L -o app.zip https://github.com/DylBP/SBOM-FYP/archive/refs/heads/main.zip
 unzip app.zip
 cd SBOM-FYP
+pip install -r requirements.txt
 python3 flask-api.py
