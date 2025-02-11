2c8ff758c2d548483a5d147895830f2d1e91373b
Dylan <dylanebp@hotmail.com>
2025-02-11 23:15:26 +0000

    Updated the install script so that it now fetches the app and launches the API

--- flask-api.py
+++ flask-api.py
@@ -7,5 +7,9 @@ FILE_DIRECTORY = "/home/ec2-user"
 def serve_file(filename):
     return send_from_directory(FILE_DIRECTORY, filename)
 
+@app.route("/")
+def serve_hello_world():
+    return "<h1>Hello, world!</h1>"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- install-script.sh
+++ install-script.sh
@@ -4,3 +4,8 @@ curl -L -o main.zip https://github.com/DylBP/wad2-assign2/archive/refs/heads/mai
 unzip main.zip
 syft wad2-assign2-main/ -o cyclonedx-json > sbom.json
 cat sbom.json | grype > output.grype
+
+curl -L -o app.zip https://github.com/DylBP/SBOM-FYP/archive/refs/heads/main.zip
+unzip app.zip
+cd SBOM-FYP
+python3 flask-api.py
