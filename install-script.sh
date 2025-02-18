aa2deebd572dd40a8e4d7c5cd35ff2a0e784dac2
Dylan <20099082@mail.wit.ie>
2025-02-18 16:46:15 +0000

    Modified the install script to work with Node

--- install-script.sh
+++ install-script.sh
@@ -1,6 +1,12 @@
 # Install Syft and Grype
-curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sudo sh -s -- -b /usr/local/bin
-curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sudo sh -s -- -b /usr/local/bin
+mkdir -p /home/ec2-user/.local/bin
+
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /home/ec2-user/.local/bin
+curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /home/ec2-user/.local/bin
+
+# Add binaries to PATH
+echo 'export PATH=$HOME/.local/bin:$PATH' >> /home/ec2-user/.bashrc
+source /home/ec2-user/.bashrc
 
 # Download and process wad2-assign2 repo
 curl -L -o main.zip https://github.com/DylBP/wad2-assign2/archive/refs/heads/main.zip
@@ -13,7 +19,9 @@ cd ..
 # Download and run SBOM-FYP Flask API
 curl -L -o app.zip https://github.com/DylBP/SBOM-FYP/archive/refs/heads/main.zip
 unzip app.zip
-cd SBOM-FYP-main
-sudo yum install -y pip
-pip install -r requirements.txt
-python3 flask-api.py
+cd SBOM-FYP-main/api
+npm install
+npm install -g pm2
+pm2 start ExpressAPI.js --name sbom-api
+pm2 startup
+
