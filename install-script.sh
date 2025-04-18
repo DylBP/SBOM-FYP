1d431d0ef99b828b5f537018507956592be28425
Dylan <20099082@mail.wit.ie>
2025-04-18 20:00:39 +0100

    Removed docker from install-script and moved it to userdata script on the EC2 userdata script. This is because Docker requires sudo to be started and enabled

--- install-script.sh
+++ install-script.sh
@@ -32,19 +32,6 @@ curl -L -o app.zip https://github.com/DylBP/SBOM-FYP/archive/refs/heads/main.zip
 unzip app.zip
 cd SBOM-FYP-main/api
 
-# Install Docker (only to pull and scan container images from dockerhub - allows user to specify remote image instead of local OCI)
-yum install -y docker
-
-# Start Docker service
-systemctl start docker
-systemctl enable docker
-
-# Add ec2-user to docker group
-usermod -aG docker ec2-user
-
-# Restart the shell to apply group changes
-echo "newgrp docker" >> /home/ec2-user/.bashrc 
-
 # Install API dependencies
 npm install
 
